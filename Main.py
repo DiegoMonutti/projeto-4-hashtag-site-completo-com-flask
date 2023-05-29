14caa387b99b07ae3034d38e88d63a9aa71a3e54
Diego Monutti <diego_monutti@outlook.com>
2023-05-29 18:40:23 -0300

    Implementar e estilizar formulários

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
 lista_usuarios = ["Diego", "Ciclano", "Fulano", "Beltrano", "Manolo"]
+
+app.config['SECRET_KEY'] = 'db457e5d385c1b7a1405355c6adc0866'
 
 @app.route("/")
 def home():
@@ -15,6 +18,12 @@ def contato():
 @app.route("/usuarios")
 def usuarios():
     return render_template("usuarios.html", lista_usuarios=lista_usuarios)
+
+@app.route("/login")
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template("login.html", form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
     app.run(debug=True)
