8967b3d925f870abab55cbde662038aa39a05d5a
Diego Monutti <diego_monutti@outlook.com>
2023-05-31 17:49:53 -0300

    Adicionar mensagem de sucesso no Login e Criar Conta

--- forms.py
+++ forms.py
@@ -4,14 +4,14 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
-    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    email_criarconta = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha_criarconta = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha_criarconta')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    email_login = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha_login = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
     
