7d332f70eff224ce5417fc3d097307f03c50f993
Diego Monutti <diego_monutti@outlook.com>
2023-05-31 18:57:42 -0300

    Adicionar mensagem de erro no Login e Criar Conta

--- forms.py
+++ forms.py
@@ -3,15 +3,15 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCriarConta(FlaskForm):
-    username = StringField('Nome de Usuário', validators=[DataRequired()])
-    email_criarconta = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha_criarconta = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
-    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha_criarconta')])
+    username = StringField('Nome de Usuário', validators=[DataRequired(message='Digite um nome de usuário válido')])
+    email_criarconta = StringField('E-mail', validators=[DataRequired(), Email(message='Digite um endereço de e-mail válido')])
+    senha_criarconta = PasswordField('Senha', validators=[DataRequired(), Length(6,20, message='Digite uma senha com 6 à 20 caracteres')])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(message='Senha não corresponde com a anterior'), EqualTo('senha_criarconta')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
 class FormLogin(FlaskForm):
-    email_login = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha_login = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    email_login = StringField('E-mail', validators=[DataRequired(), Email(message='Digite um endereço de e-mail válido')])
+    senha_login = PasswordField('Senha', validators=[DataRequired(), Length(6,20, message='Digite sua senha corretamente')])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
     
