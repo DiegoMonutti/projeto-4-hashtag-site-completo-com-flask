7ff1526cfc6c20af2fa79e24085243abdf23ec51
Diego Monutti <diego_monutti@outlook.com>
2023-05-31 17:54:49 -0300

    Adicionar mensagem de sucesso no Login e Criar Conta

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
@@ -19,10 +19,19 @@ def contato():
 def usuarios():
     return render_template("usuarios.html", lista_usuarios=lista_usuarios)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucesso no e-mail: {form_login.email_login.data}', 'alert-success')
+        return redirect(url_for('home'))
+
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada com sucesso no e-mail: {form_criarconta.email_criarconta.data}', 'alert-success')
+        return redirect(url_for('home'))
+    
     return render_template("login.html", form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
